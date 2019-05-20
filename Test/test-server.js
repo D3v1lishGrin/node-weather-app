ff1d77442851611cc419f4f6c87bac255abc642c
D3v1lishGrin <jerome.oliver@yahoo.de>
2019-05-20 21:02:47 +0200

    Added another set of tests

--- Test/test-server.js
+++ Test/test-server.js
@@ -6,11 +6,21 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
+it('should return code 404', (done) => {
+    chai.request(server)
+        .get('/cities')
+        .end((err, res) => {
+            res.should.have.status(404);
+            res.should.be.json;
 
-it ('should return the city with the correct properties', (done) =>{
+            done();
+        });
+});
+
+it('should return the city with the correct properties', (done) => {
     chai.request(server)
         .get('/cities/2873891/weather')
-        .end((err, res)=>{
+        .end((err, res) => {
             res.should.have.status(200);
             res.should.be.json;
             res.body.should.have.property('sunrise');
@@ -24,4 +34,26 @@ it ('should return the city with the correct properties', (done) =>{
 
             done();
         });
+});
+
+it('should return error 404 because of missing lat parameter', (done) => {
+    chai.request(server)
+        .get('/cities/?lng=8.46')
+        .end((err, res) => {
+            res.should.have.status(404);
+            res.should.be.json;
+
+            done();
+        });
+});
+
+it('should return error 404 because of missing lng parameter', (done) => {
+    chai.request(server)
+        .get('/cities/?lat=49.48')
+        .end((err, res) => {
+            res.should.have.status(404);
+            res.should.be.json;
+
+            done();
+        })
 });
--- api/routes/cities.js
+++ api/routes/cities.js
@@ -2,22 +2,25 @@ const express = require('express');
 const router = express.Router();
 const request = require('request');
 
+
 router.get('/', (req, res) => {
     const lat = req.query.lat;
     const lng = req.query.lng;
-    request('https://api.openweathermap.org/data/2.5/find?lat='+ lat +'&lon='+ lng +'&APPID=062c05ac6bdf37eca9574c43268519b4',
-        (error, response, body) =>{
-            if(lat === undefined || lng === undefined){
+
+    request('https://api.openweathermap.org/data/2.5/find?lat=' + lat + '&lon=' + lng + '&APPID=062c05ac6bdf37eca9574c43268519b4',
+        (error, response, body) => {
+
+            //handle if there are no parameters provided
+            if (lat === undefined || lng === undefined) {
                 res.status(404).json({
-                    "code":"NotFoundError",
-                    "message":"not found"
+                    "code": "NotFoundError",
+                    "message": "not found"
                 });
-            }
-            else{
+            } else {
                 const parsedBody = JSON.parse(body);
                 let ret = [];
                 for (let i in parsedBody.list) {
-                    ret.push({id : parsedBody.list[i].id, name : parsedBody.list[i].name});
+                    ret.push({id: parsedBody.list[i].id, name: parsedBody.list[i].name});
                 }
 
                 res.status(200).json({
@@ -29,14 +32,15 @@ router.get('/', (req, res) => {
 
 router.get('/:cityId', (req, res) => {
     const cityId = req.params.cityId;
-    request('https://api.openweathermap.org/data/2.5/weather?id='+cityId+'&APPID=062c05ac6bdf37eca9574c43268519b4',
-        (error, response, body) =>{
+    request('https://api.openweathermap.org/data/2.5/weather?id=' + cityId + '&APPID=062c05ac6bdf37eca9574c43268519b4',
+        (error, response, body) => {
 
-            if(error){
-                //res.status(404).json();
-                res.send('An error occured');
-            }
-            else{
+            if (error) {
+                res.status(404).json({
+                    'message':'not found',
+                    'code':'NotFoundError'
+                });
+            } else {
                 const parsedBody = JSON.parse(body);
 
                 res.status(200).json({
@@ -52,13 +56,15 @@ router.get('/:cityId', (req, res) => {
 
 router.get('/:cityId/weather', (req, res) => {
     const cityId = req.params.cityId;
-    request('https://api.openweathermap.org/data/2.5/weather?id='+cityId+'&APPID=062c05ac6bdf37eca9574c43268519b4',
-        (error, response, body) =>{
+    request('https://api.openweathermap.org/data/2.5/weather?id=' + cityId + '&APPID=062c05ac6bdf37eca9574c43268519b4',
+        (error, response, body) => {
 
-            if(error){
-                res.send('An error occured');
-            }
-            else{
+            if (error) {
+                res.status(404).json({
+                    'message':'not found',
+                    'code':'NotFoundError'
+                });
+            } else {
                 const parsedBody = JSON.parse(body);
 
                 res.status(200).json({
